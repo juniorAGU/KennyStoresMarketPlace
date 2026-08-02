d9f9e05550e831121437b0f877513ae353e75146
juniorAGU <perryfresh25@gmail.com>
2026-08-01 21:27:58 -0700

    cors fixed

--- server/server.js
+++ server/server.js
@@ -29,7 +29,7 @@ const app = express();
 app.use(helmet());
 
 app.use(cors({
-    origin: ["https://kenny-stores-market-place.vercel.app/"],
+    origin: ["https://kenny-stores-market-place.vercel.app"],
     credentials: true
 }));
 ConnectDB();
